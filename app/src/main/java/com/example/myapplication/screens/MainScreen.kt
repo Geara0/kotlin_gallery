80660a6c0109d8c35849533ac5f119db5f2853d0
vladslav008 <vladslav008@mail.ru>
2023-10-31 15:44:22 +0500

    changed the Select button

--- app/src/main/java/com/example/myapplication/screens/MainScreen.kt
+++ app/src/main/java/com/example/myapplication/screens/MainScreen.kt
@@ -14,6 +14,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -50,17 +51,29 @@ fun MainScreen(navController: NavController) {
                     title = { Text("Kotlin gallery") },
                 )
             },
+            bottomBar = {
+                BottomAppBar(
+                    content = {
+                        Button(
+                            onClick = {
+                                b.launch(Uri.parse("image/*"))
+                            },
+                            modifier = Modifier.weight(1f),
+                            content = {
+                                Column {
+                                    Text(text = "Select")
+                                }
+                            }
+                        )
+                    }
+                )
+            },
             content = { padding ->
                 Surface(
                     modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background
                 ) {
                     Box(modifier = Modifier.padding(padding)) {
                         Column {
-                            Button(onClick = {
-                                b.launch(Uri.parse("image/*"))
-                            }) {
-                                Text(text = "Select")
-                            }
                             result.value?.let {
                                 if (Build.VERSION.SDK_INT < 28) {
                                     val a = DocumentFile.fromTreeUri(context, result.value!!)
