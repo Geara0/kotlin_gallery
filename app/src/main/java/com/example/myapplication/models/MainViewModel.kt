209c5166fbc4b55dced5c9008051484a3216428b
Akulin_Dmitry <newmitya2@gmail.com>
2023-12-03 12:17:50 +0500

    saving the filtered image and replacing the original one

--- app/src/main/java/com/example/myapplication/models/MainViewModel.kt
+++ app/src/main/java/com/example/myapplication/models/MainViewModel.kt
@@ -10,21 +10,28 @@ import androidx.documentfile.provider.DocumentFile
 import androidx.lifecycle.ViewModel
 
 class MainViewModel : ViewModel() {
-    var bitmapList: List<Bitmap>? = null
-        private set
-
+    var bitmapList: MutableList<Bitmap>? = null
+    var fileNames: List<Uri>? = null
+    var rootDirUri: Uri? = null
     fun setBitmapList(context: Context, uri: Uri) {
         if (Build.VERSION.SDK_INT < 28) {
+            rootDirUri = uri
             val a = DocumentFile.fromTreeUri(context, uri)
-            bitmapList = a?.listFiles()
-                ?.map { e ->
-                    MediaStore.Images.Media.getBitmap(
-                        context.contentResolver,
-                        e.uri
-                    )
-                }
+            fileNames = a?.listFiles()?.map { file ->
+                file.uri
+            }
+
+            bitmapList = a?.listFiles()?.map { e ->
+                MediaStore.Images.Media.getBitmap(
+                    context.contentResolver,
+                    e.uri
+                )
+            }?.toMutableList()
         } else {
             val a = DocumentFile.fromTreeUri(context, uri)
+            fileNames = a?.listFiles()?.map { file ->
+                file.uri
+            }
 
             bitmapList = a?.listFiles()?.map { e ->
                 ImageDecoder.decodeBitmap(
@@ -32,7 +39,7 @@ class MainViewModel : ViewModel() {
                         context.contentResolver, e.uri
                     )
                 )
-            }
+            }?.toMutableList()
         }
     }
 }
